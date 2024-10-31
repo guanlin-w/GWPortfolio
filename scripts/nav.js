45d5385c986ca57fdabb0679699ae6ac44d0a891
guanlin-w <36206304+guanlin-w@users.noreply.github.com>
2024-10-31 15:11:28 -0500

    Working on Navbar

---
+++ scripts/nav.js
@@ -0,0 +1,10 @@
+// https://stackoverflow.com/a/68909928
+console.log("Hellow")
+fetch('./nav.html')
+.then(res=> res.text())
+.then(text => {
+    let oldelem = document.querySelector("script#replace_with_navbar");
+    let newelem = document.createElement("div");
+    newelem.innerHTML = text;
+    oldelem.parentNode.replaceChild(newelem,oldelem);
+})  
